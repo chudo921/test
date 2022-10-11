c950f5d4f3c01ed6261ce3de5f0fcae708abf231
Oleg Polyakov <polyakov.gelo@yandex.ru>
2022-10-11 23:55:06 +0300

    добавленны 4 ветки

--- src/main.java
+++ src/main.java
@@ -1,5 +1,8 @@
 public class main {
     public static void main(String[] args) {
         System.out.println("slova suka bleat");
+        System.out.println("Пришел разъебать тут все к хуям");
+        System.out.println("Оу сука новые строчки блять");
+        System.out.println("Щас я заебучсь блять нахуй");
     }
 }
